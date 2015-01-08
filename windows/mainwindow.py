f8d30292d22da1e130a33af2826ec58bfbbb2e14
will <williangalvani@gmail.com>
2015-01-09 01:18:43 +0200

    fixed error when unnamed value is 0

--- windows/mainwindow.py
+++ windows/mainwindow.py
@@ -167,7 +167,7 @@ class MainWindow(QtGui.QMainWindow):
                         self.dataSets[datasetName_].curve.attach(self.qwtPlot)
                         self.dataSets[datasetName_].setColor(self.getColor(datasetName_))
                 for i in range(len(points)):
-                    if points[i]:
+                    if points[i] is not None:
                         self.dataSets[datasetName + '.['+str(i)+']'].addPoint(points[i])
 
     def setupPlot(self):
